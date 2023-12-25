1c1e2a6c420d92bb4c1b2ad3b7ef71520bc273ce
Nino <nino.godziashvili@gmail.com>
2023-12-25 18:19:55 +0400

    Added Skeleton for the list

--- DecouplingNetworkLayer/View/ContentView.swift
+++ DecouplingNetworkLayer/View/ContentView.swift
@@ -33,7 +33,7 @@ struct ContentView: View {
                     }
                     .navigationTitle(Constants.TitleData.donorNames)
                 } else {
-                    ProgressView()
+                    SkeletonListView()
                 }
             }
         }
---
+++ DecouplingNetworkLayer/View/SkeletonListView.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftUI
+
+struct SkeletonListView: View {
+    var body: some View {
+        List {
+            ForEach(0..<10, id: \.self) { _ in
+                SkeletonRowView()
+            }
+        }
+    }
+}
+
+struct SkeletonRowView: View {
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.gray.opacity(0.2))
+                .frame(height: 25)
+            
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: 4)
+                .fill(Color.gray.opacity(0.2))
+                .frame(width: 25, height: 20)
+        }
+        .padding()
+    }
+}
