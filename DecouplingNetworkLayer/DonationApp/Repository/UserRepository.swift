50545ca1b47fac36b2395654199a16fc72322c8d
Nino Godziashvili <nino.godziashvili@gmail.com>
2024-03-20 11:46:05 +0100

    Login functionality additional changes

--- DecouplingNetworkLayer/DonationApp/Repository/UserRepository.swift
+++ DecouplingNetworkLayer/DonationApp/Repository/UserRepository.swift
@@ -2,5 +2,7 @@ import Foundation
 
 class UserRepository {
     func login(username: String, password: String, completion: @escaping (Bool) -> Void) {
+        let loginSuccess = username == "" && password == ""
+        completion(loginSuccess)
     }
 }
