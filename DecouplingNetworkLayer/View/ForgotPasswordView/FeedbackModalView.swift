4e25fa131919b2a7c04339acd53c7e551614f894
Nino Godziashvili <nino.godziashvili@gmail.com>
2024-03-29 15:17:02 +0100

    Add animation for modal sheet

--- DecouplingNetworkLayer/View/ForgotPasswordView/FeedbackModalView.swift
+++ DecouplingNetworkLayer/View/ForgotPasswordView/FeedbackModalView.swift
@@ -7,29 +7,42 @@
 
 import Foundation
 import SwiftUI
+import Lottie
 
 struct FeedbackModalView: View {
-    @ObservedObject var viewModel: FeedbackModalViewModel
-    
-    // Mark: Body
+    @ObservedObject var viewModel = FeedbackModalViewModel()
+    @State private var modalHeight: CGFloat = .zero
     
     var body: some View {
-        VStack {
-            Text(viewModel.message)
-                .font(.title)
-                .padding()
-            
-            Button(action: {
-                viewModel.navigateToLoginPage()
-            }) {
-                Text("Go to Login Page")
+        GeometryReader { geometry in
+            VStack(spacing: 10) {
+                LottieView(animationName: "successAnimation")
+                    .frame(maxWidth: 150, maxHeight: 150)
+                    .scaleEffect(0.5)
+                    .aspectRatio(contentMode: .fill)
                     .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color(red: 53/255, green: 53/255, blue: 53/255))
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                Text(viewModel.message)
+                    .font(.system(size: 16))
+                    .padding()
+                
+                Button(action: {
+                    viewModel.navigateToLoginPage()
+                }) {
+                    Text("Go to Login Page")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color(red: 53/255, green: 53/255, blue: 53/255))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding()
             }
-            .padding()
         }
+    }
+}
+
+struct FeedbackModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedbackModalView()
     }
 }
